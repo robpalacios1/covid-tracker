002dda4530d02c10e0a3adf5252ef411790cf832
roberto <robpalacios1@hotmail.com>
2020-12-04 14:41:53 -0500

    add InfoBox.jsx component

---
+++ src/components/InfoBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, CardContent, Typography} from '@material-ui/core';
+
+const Infobox = (props) => {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                {/** Title i.e Coronavirus */}
+                <Typography className="infoBox__title" color="textSecondary">
+                    {props.title}
+                </Typography>
+
+                {/** Number os cases */}
+                <h2 className="infoBox__cases">{props.cases}</h2>
+
+                {/** Total of cases */}
+                <Typography className="infoBox__total" color="textSecondary">
+                    {props.total} Total
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Infobox
