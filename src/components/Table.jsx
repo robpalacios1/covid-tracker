0f7c24be3d352617eacadfffbe859b4e30c7acaa
roberto <robpalacios1@hotmail.com>
2020-12-04 18:14:07 -0500

    add Table.jsx component

---
+++ src/components/Table.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Table.css';
+
+const Table = (props) => {
+    return (
+        <div className="table">
+            {props.countries.map(({ country, cases }) => (
+                <tr>
+                    <td>{country}</td>
+                    <td><strong>{cases}</strong></td>
+                </tr>
+            ))}
+        </div>
+    )
+}
+
+export default Table
