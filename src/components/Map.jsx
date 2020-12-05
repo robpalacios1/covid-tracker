aaeb60505bf6d11e510fe836dff7c7192ef710d9
roberto <robpalacios1@hotmail.com>
2020-12-05 14:11:51 -0500

    insert props for props.center and props.zoom

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
 import './Map.css'
 
-const Map = ({ center, zoom }) => {
+const Map = (props) => {
     return (
         <div className="map">
-            <LeafletMap center={center} zoom={zoom}>
+            <LeafletMap center={props.center} zoom={props.zoom}>
                 <TileLayer
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
