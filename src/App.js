35934cef74df9388c844b721568a56db324a4cdc
roberto <robpalacios1@hotmail.com>
2020-12-04 19:22:37 -0500

    install chart.js an import LineGraph.jsx component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InfoBox from './components/InfoBox'
 import Map from './components/Map'
 import Table from './components/Table'
 import { sortData } from './components/util'
+import LineGraph from './components/LineGraph';
 import './App.css';
 
 function App() {
@@ -106,7 +107,7 @@ function App() {
           <h3>Lives Cases by country</h3>
           <Table countries={tableData}/>
           <h3>Worldwide new cases</h3>
-          {/** Graph */}
+          <LineGraph />
         </CardContent>
         {/**Table */}
         {/**Graph */}
