18c48413c50a936c37b148987218c449500a890e
roberto <robpalacios1@hotmail.com>
2020-12-04 19:21:34 -0500

    add LineGraph.jsx component

---
+++ src/components/LineGraph.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { Line } from 'react-chartjs-2'
+
+const LineGraph = () => {
+
+    const [data, setData] = useState({});
+
+    // https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1>I am a graph</h1>
+            {/*<Line data options/>*/}
+        </div>
+    )
+}
+
+export default LineGraph
