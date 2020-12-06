ff035377931d87b3e70cc6604e7eb584cad1cf84
roberto <robpalacios1@hotmail.com>
2020-12-06 17:32:24 -0500

    import Circle and Popup

--- src/components/util.jsx
+++ src/components/util.jsx
@@ -30,6 +30,10 @@ export const sortData = (data) => {
     return sortedData;
 }
 
+export const prettyPrintStat = (stat) =>
+    stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
+
 //DRAW circles on the map
 export const showDataOnMap = (data, casesType='cases') => (
     data.map(country => (
@@ -42,9 +46,31 @@ export const showDataOnMap = (data, casesType='cases') => (
                 Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
             }
         >
-        <Popup>
-            <h1>I am a Popup</h1>
-        </Popup>
+            <Popup>
+                <div className="info-container">
+                    <div
+                        className="info-flag"
+                        style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+                    />
+                    <div
+                        className="info-name">
+                        <strong>{country.country}</strong>
+                    </div>
+                    <div
+                        className="info-confirmed">
+                        <strong>Cases:</strong>
+                        {numeral(country.cases).format("0,0")}
+                    </div>
+                    <div className="info-recovered">
+                        <strong>Recovered:</strong>
+                        {numeral(country.recovered).format("0,0")}
+                    </div>
+                    <div className="info-deaths">
+                        <strong>Deaths:</strong>
+                        {numeral(country.deaths).format("0,0")}
+                    </div>
+                </div>
+            </Popup>
         </Circle>
     ))
 );
