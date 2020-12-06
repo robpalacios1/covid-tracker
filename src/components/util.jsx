a3bbd11abf422286f224621dc9a35d9154936351
roberto <robpalacios1@hotmail.com>
2020-12-06 15:30:05 -0500

    add functionality of draw circles on the map

--- src/components/util.jsx
+++ src/components/util.jsx
@@ -1,3 +1,22 @@
+import React from 'react';
+import numeral from 'numeral';
+import {Circle, Popup} from 'react-leaflet';
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        multiplier: 300,
+    },
+    recovered: {
+        hex: "#7DD71D",
+        multiplier: 600,
+    },
+    deaths: {
+        hex: "#FB4443",
+        multiplier: 1000,
+    },
+};
+
 export const sortData = (data) => {
     const sortedData = [...data];
 
@@ -10,3 +29,23 @@ export const sortData = (data) => {
     })
     return sortedData;
 }
+
+//DRAW circles on the map
+export const showDataOnMap = (data, casesType='cases') => (
+    data.map(country => (
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }
+        >
+        <Popup>
+            <h1>I am a Popup</h1>
+        </Popup>
+        </Circle>
+    ))
+);
+
