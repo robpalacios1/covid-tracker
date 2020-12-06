54df63257eb81146c5f5a65233bd96eed6b3df9e
roberto <robpalacios1@hotmail.com>
2020-12-06 15:33:12 -0500

    add useState of mapCountries to show it

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ function App() {
   const[country, setCountry] = useState('worldwide');
   const[countryInfo, setCountryInfo] = useState({});
   const[tableData, setTableData] = useState([]);
-  const[mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796})
+  const[mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 })
   const[mapZoom, setMapZoom] = useState(3)
+  const[mapCountries, setMapCountries] = useState([])
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -39,6 +40,7 @@ function App() {
 
         const sortedData = sortData(data)
         setTableData(sortedData);
+        setMapCountries(data);
         setCountries(countries);
       })
     }
@@ -104,6 +106,7 @@ function App() {
         </div>
 
         <Map
+          countries={mapCountries}
           center={mapCenter}
           zoom={mapZoom}
         />
